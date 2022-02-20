23c3bb289f4cd408ca485fa49fae85ded3be4041
Sophia <67487404+sophiaahh@users.noreply.github.com>
2022-02-19 22:11:18 -0500

    Create TreeIsFullException.java

---
+++ TreeIsFullException.java
@@ -0,0 +1,14 @@
+package implementation;
+
+/**
+ * Thrown when a tree is full. Do not modify this class.
+ * 
+ * 
+ */
+public class TreeIsFullException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public TreeIsFullException(String message) {
+		super(message);
+	}
+}
