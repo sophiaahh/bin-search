5f05c0c6a5bafd5feecd4f3e7c5b63d21bfa7e54
Sophia <67487404+sophiaahh@users.noreply.github.com>
2022-02-19 22:09:40 -0500

    Create keypairs.java

---
+++ keypairs.java
@@ -0,0 +1,28 @@
+package implementation;
+
+/**
+ * 
+ * Use to keep track of key/value pairs retrieved from the tree. Do not modify
+ * this class.
+ * 
+ * 
+ * @param <K>
+ * @param <V>
+ */
+public class KeyValuePair<K, V> {
+	private K key;
+	private V value;
+
+	public KeyValuePair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+}
